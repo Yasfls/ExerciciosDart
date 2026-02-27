fda602b891926353f736c4a34da4e2a317cb5dc3
Yasmin <yasminflsilva@gmail.com>
2026-02-26 22:04:23 -0300

    commit

---
+++ exercicio_10.dart
@@ -0,0 +1,8 @@
+void main() {
+Set<int> numeros1 = {1, 2, 3, 4, 5};
+Set<int> numeros2 = {4, 5, 6, 7, 8};
+
+print(numeros1.union(numeros2));
+print(numeros1.intersection(numeros2)); 
+print(numeros1.difference(numeros2));
+}
